d3e34591529910767654400e16005b5c50144a3b
ciago-la <ciagolm@gmail.com>
2025-08-14 13:44:28 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-self.addEventListener('fetch',()=>{});
+const CACHE='alter-missions-v1';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); });
+self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE && caches.delete(k))))); });
+self.addEventListener('fetch',e=>{ e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request))); });
